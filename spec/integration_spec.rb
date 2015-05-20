f9611eca9fb860c215d98a4e06fe8fc1b4e8b1bd
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2015-05-20 11:48:30 +0900

    fix example

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,6 +2,4 @@ require "spec_helper"
 RSpec.describe "chibineko/rspec" , ignore: true do
   extend Chibineko::RSpec::Helper
   include_chibineko("https://chibineko.jp/t/_op4xxVZqT-8KMd4S6FTgw")
-  xit { expect(nil).to be_nil}
-  it { pending "some reason" }
 end
