a8adcb9994617bf86ab928fe902792c55cb26adc
Kousuke TANIGUCHI <kousuke.taniguchi@gmail.com>
2015-05-23 13:48:20 +0900

    add examples with mocke

--- spec/chibineko/rspec/helper_spec.rb
+++ spec/chibineko/rspec/helper_spec.rb
@@ -3,4 +3,26 @@ require 'spec_helper'
 RSpec.describe Chibineko::RSpec::Helper do
   it { expect(described_class).to be_a(Module) }
   it { expect(described_class).to be_public_method_defined(:include_chibineko) }
+  describe "#include_chibineko" do
+    let(:testcase_url) {"http://example.com/t/TESTCASE_ID"}
+    subject do
+      klass = Class.new
+      klass.extend described_class
+      klass
+    end
+
+    it "#include_chibineko" do
+      testcase = double('testcase')
+      exporter = double('exporter')      
+
+      expect(Chibineko::TestCase).to receive(:retrieve).with(testcase_url) { testcase }
+      expect(Chibineko::RSpec::ExampleGroupExporter)
+        .to(receive(:new).with(subject, testcase) { exporter })
+
+      expect(exporter).to receive(:export).with(no_args)
+      
+      expect { subject.include_chibineko(testcase_url) }.not_to raise_error
+    end
+    
+  end
 end
